a3310958a4c4a5e5974de013250f775dfd498d29
Jkettler13 <jkettler13@gmail.com>
2024-08-24 11:46:07 -0500

    Give MedicineBlock give regen 3 onUse Updated marble_block.png

--- src/main/java/net/bbb13/friendsforgermod/block/custom/MedicineBlock.java
+++ src/main/java/net/bbb13/friendsforgermod/block/custom/MedicineBlock.java
@@ -1,13 +1,28 @@
 package net.bbb13.friendsforgermod.block.custom;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class MedicineBlock extends Block {
+
 
     public MedicineBlock(Settings settings) {
         super(settings);
     }
 
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (world.isPlayerInRange(pos.getX(), pos.getY(), pos.getZ(), 5.0)) {
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 240, 2));
+        }
+        return ActionResult.SUCCESS;
 
-
+    }
 }
