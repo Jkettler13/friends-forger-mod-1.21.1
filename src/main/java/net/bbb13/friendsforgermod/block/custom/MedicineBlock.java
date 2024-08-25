be9e18605dae201361181b3c2ea9517254e4d230
Jkettler13 <jkettler13@gmail.com>
2024-08-25 04:39:12 -0500

    Added tooltips

--- src/main/java/net/bbb13/friendsforgermod/block/custom/MedicineBlock.java
+++ src/main/java/net/bbb13/friendsforgermod/block/custom/MedicineBlock.java
@@ -5,10 +5,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class MedicineBlock extends Block {
 
@@ -20,9 +27,14 @@ public class MedicineBlock extends Block {
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (world.isPlayerInRange(pos.getX(), pos.getY(), pos.getZ(), 5.0)) {
-            player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 240, 2));
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 4));
         }
         return ActionResult.SUCCESS;
 
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.friendsforgermod.medicine_block.tooltip").formatted(Formatting.GREEN));
     }
 }
--- src/main/java/net/bbb13/friendsforgermod/item/custom/DrinkItem.java
+++ src/main/java/net/bbb13/friendsforgermod/item/custom/DrinkItem.java
@@ -3,7 +3,12 @@ package net.bbb13.friendsforgermod.item.custom;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.UseAction;
+
+import java.util.List;
 
 public class DrinkItem extends Item {
 
@@ -13,5 +18,10 @@ public class DrinkItem extends Item {
     @Override
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.DRINK;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.friendsforgermod.energy_drink.tooltip").formatted(Formatting.YELLOW));
     }
 }
