4e31a187acfee2e6c2344397ce6dd623d5cb96b1
Jkettler13 <jkettler13@gmail.com>
2024-08-27 23:52:35 -0500

    Added effect to WebGunItem

--- src/main/java/net/bbb13/friendsforgermod/item/custom/WebGunItem.java
+++ src/main/java/net/bbb13/friendsforgermod/item/custom/WebGunItem.java
@@ -1,8 +1,7 @@
 package net.bbb13.friendsforgermod.item.custom;
 
 import net.bbb13.friendsforgermod.sound.ModSounds;
-import net.fabricmc.fabric.api.event.AutoInvokingEvent;
-import net.minecraft.block.BlockState;
+
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -10,13 +9,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.sound.SoundCategory;
+
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
+
 import net.minecraft.util.Formatting;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
 
 import java.util.List;
 
@@ -29,6 +26,13 @@ public class WebGunItem extends Item {
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         target.playSound(ModSounds.WEB_GUN_HIT, 100.0f, 1.0f);
+        target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,40, 255));
+        target.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 40, 255));
+
+        if (attacker instanceof PlayerEntity){
+            attacker.sendMessage(Text.of(attacker.getName().getString() + " stunned " + target.getName().getString() + "!"));
+        }
+
 
         return false;
     }
